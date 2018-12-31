a14225269eaab4ca86a868724dcf83724e4ec096
electricgearnuts <jeff.hapke@gmail.com>
2018-12-31 23:08:59 +0000

    Made some changes to ChiliPeppr myWorkspace using Cloud9

--- workspace.js
+++ workspace.js
@@ -41,7 +41,7 @@ cpdefine("inline:com-chilipeppr-workspace-cnc-cam", ["chilipeppr_ready"], functi
          */
         id: "com-chilipeppr-workspace-cnc-cam", // Make the id the same as the cpdefine id
         name: "Workspace / cnc-cam", // The descriptive name of your widget.
-        desc: `A ChiliPeppr Workspace cnc-cam.`,
+        desc: `A ChiliPeppr Workspace to run the cnc-cam.`,
         url: "(auto fill by runme.js)", // The final URL of the working widget as a single HTML file with CSS and Javascript inlined. You can let runme.js auto fill this if you are using Cloud9.
         fiddleurl: "(auto fill by runme.js)", // The edit URL. This can be auto-filled by runme.js in Cloud9 if you'd like, or just define it on your own to help people know where they can edit/fork your widget
         githuburl: "(auto fill by runme.js)", // The backing github repo
@@ -137,6 +137,55 @@ cpdefine("inline:com-chilipeppr-workspace-cnc-cam", ["chilipeppr_ready"], functi
                         }
                     );
                 }
+            );
+        },
+        
+        /**
+         * Load the camera widget via chilipeppr.load()
+         */
+        loadCameraWidget: function(callback) {
+
+            var that = this;
+
+            chilipeppr.load(
+              "#myDivWidgetCam",
+              "http://raw.githubusercontent.com/jhapke/widget-cam/master/auto-generated-widget.html",
+              function() {
+                // Callback after widget loaded into #myDivWidgetCam
+                // Now use require.js to get reference to instantiated widget
+                cprequire(
+                  ["inline:com-chilipeppr-widget-cam"], // the id you gave your widget
+                  function(myObjWidgetCam) {
+                    // Callback that is passed reference to the newly loaded widget
+                    console.log("Widget / Cam just got loaded.", myObjWidgetCam);
+                    myObjWidgetCam.init();
+                  }
+                );
+              }
+            );
+        },
+        /**
+         * Load the Serial Port JSON Server widget via chilipeppr.load()
+         */
+        loadXBOXWidget: function(callback) {
+
+            var that = this;
+
+            chilipeppr.load(
+              "#xbox-instance",
+              "https://raw.githubusercontent.com/chilipeppr/widget-xbox/master/auto-generated-widget.html",
+              function() {
+                // Callback after widget loaded into #myDivWidgetXbox
+                // Now use require.js to get reference to instantiated widget
+                cprequire(
+                  ["inline:xbox-instance"], // the id you gave your widget
+                  function(myObjWidgetXbox) {
+                    // Callback that is passed reference to the newly loaded widget
+                    console.log("Widget / Xbox just got loaded.", myObjWidgetXbox);
+                    myObjWidgetXbox.init();
+                  }
+                );
+              }
             );
         },
         /**
